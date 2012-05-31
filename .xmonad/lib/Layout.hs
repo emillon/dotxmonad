3d707c321abc92a3da940231e50422b8074df30b
Etienne Millon <etienne.millon@gmail.com>
2012-05-31 11:26:23 +0200

    Remove layout specific to workspace 2

--- .xmonad/lib/Layout.hs
+++ .xmonad/lib/Layout.hs
@@ -6,22 +6,11 @@ module Layout (myLayoutHook) where
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
-import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Reflect
-import XMonad.Layout.Tabbed
 
 myLayoutHook = smartBorders . avoidStruts $ myLayout
 
-myTabbed = tabbed shrinkText myTheme
-  where
-    myTheme = defaultTheme {   activeColor = "black"
-                           , inactiveColor = "black"
-                           }
-
-
 myLayout =
-  onWorkspace "2"  (myTabbed ||| Full) $
-  {-onWorkspace "8"  (withIM (1/5) (Role "buddy_list") Full ||| Full) $-}
     reflectHoriz tiled ||| Full
       where
         tiled    = Tall nmaster delta ratio
